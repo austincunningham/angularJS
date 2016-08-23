221201c63bf384c51f3639b40627ec91a9867732
austincunningham <austincunningham@hotmail.com>
2016-08-23 14:30:00 +0100

    binding CSS classes

---
+++ Binding CSS classes/js/root.js
@@ -0,0 +1,11 @@
+/**
+ * Created by austin on 23/08/2016.
+ */
+
+angular.module("root", [])
+    .controller("index", ["$scope", function($scope) {
+      $scope.value = 1;
+      $scope.isBold = function () { return $scope.value % 2 === 0; };
+      $scope.isItalic = function () { return $scope.value % 3 === 0; };
+      $scope.isUnderlined = function () { return $scope.value % 5 === 0; };
+    }]);
